79b94910ce1ba031934dbd7060f92165b9345a78
dellaao <dellaoctaamelia@gmail.com>
2022-02-24 17:06:51 +0700

    add favorite

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Button from 'react-bootstrap/Button';
 import Home from './components/Home';
 import Detail from './components/Detail';
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, useNavigate} from "react-router-dom";
+import Favorite from './components/Favorite';
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
           {/* root , not found , all path the same */}
-          <Route path="/home" element={<Home/>} />
+          <Route path="*" element={<Home/>} />
           <Route path="/detail" element={<Detail/>} />
-          {/* detail */}
-          {/* fav */}
+          <Route path="/favorite" element={<Favorite/>} />
           <Route path="*" element={<Navigate to='/404' />} />
           <Route path='/404' element={<div>404</div>} />
           
